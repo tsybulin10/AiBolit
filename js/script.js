230ea40e313a6815d13059a29c827218a21f3f28
Igor Lukianov <tsybulin10@gmail.com>
2022-03-12 21:07:44 +0200

    last versiob

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.switcher__one').click(function(event) {
+		$('.switcher__one,').toggleClass('active');
+	});
+	$('.switcher__two').click(function(event) {
+		$('.switcher__two').toggleClass('active');
+	});
+	$('.switcher__three').click(function(event) {
+		$('.switcher__three').toggleClass('active');
+	});
+	$('.switcher__four').click(function(event) {
+		$('.switcher__four').toggleClass('active');
+	});
+	$('.switcher__five').click(function(event) {
+		$('.switcher__five').toggleClass('active');
+	});
+	$('.switcher__six').click(function(event) {
+		$('.switcher__six').toggleClass('active');
+	});
+});
+
