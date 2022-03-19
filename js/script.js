d8fb62c9df8c668b5f28ff3601c3a06cee07171f
Igor Lukianov <tsybulin10@gmail.com>
2022-03-19 15:32:50 +0200

    переделанное контент шапки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var count = $(".block").length, i = 1;
-setInterval(function() {
-  i < count ? i++ : i = 1;
-  $(".block").removeClass("active").eq(i - 1).addClass("active"),
-  $(".list").removeClass("active").eq(i - 1).addClass("active") }, 3000);
+// var count = $(".block").length, i = 1;
+// setInterval(function() {
+//   i < count ? i++ : i = 1;
+//   $(".block").removeClass("active").eq(i - 1).addClass("active"),
+//   $(".list").removeClass("active").eq(i - 1).addClass("active") }, 5000);
