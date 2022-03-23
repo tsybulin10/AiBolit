651e59befcaf6bc37a736044199bd00ed7707972
Igor Lukianov <tsybulin10@gmail.com>
2022-03-23 21:29:28 +0200

    добавлен бургер, адаптив

--- js/script.js
+++ js/script.js
@@ -1,3 +1,88 @@
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+
+} else {
+	document.body.classList.add('_pc');
+}
+
+
+
+const iconTitle = document.querySelector('.title__icon');
+const titleNavSub = document.querySelector('.title__navbar-sub');
+if (iconTitle) {
+	iconTitle.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconTitle.classList.toggle('_active');
+		titleNavSub.classList.toggle('_active');
+	});
+}
+
+
+
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if (iconTitle.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconTitle.classList.remove('_active');
+				titleNavSub.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
 // var count = $(".block").length, i = 1;
 // setInterval(function() {
 //   i < count ? i++ : i = 1;
